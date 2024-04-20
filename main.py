82473a5f9efb8675f51f06eeb928febb1bd29c2a
Praneet <praneetdumpa@gmail.com>
2024-04-20 16:13:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,50 @@
+import math
+
+
+def two_id_calc(shape):
+    print("\n")
+    if shape == "rectangle":
+        length = int(input("Length: "))
+        width = int(input("Width: "))
+        print("Area = length * width")
+        print("Area = ", (length * width), "sq.units")
+    elif shape == "triangle":
+        base = int(input("Base: "))
+        height = int(input("Height: "))
+        print("Area = (1/2) * base * height")
+        print("Area = ", ((0.5) * base * height), "sq.units")
+    elif shape == "trapezoid":
+        top_base = int(input("Base One: "))
+        bottom_base = int(input("Base Two: "))
+        height = int(input("Height: "))
+        print("Area = (1/2) * (base_one + base_two) * height")
+        print("Area = ", ((0.5) * (top_base + bottom_base) * height), "sq.units")
+    elif shape == "circle":
+        radius = int(input("Radius: "))
+        print("Area = pi * radius * radius")
+        print("Area = ", (math.pi * radius * radius), "sq.units")
+    print("\n")
+
+
 def main():
     two_or_three = input("Would you like to analyze a 2D or 3D shape? ")
     shape = input("Which shape would you like to analyze? ")
     two_shapes = ["rectangle", "triangle", "trapezoid", "circle"]
     three_shapes = ["rectangular prism", "cone", "cylinder", "sphere"]
 
-    if shape == "rectangle":
-        length = input("Length: ")
-        width = input("Width: ")
-        rectangle(length, width)
-    elif shape == "triangle":
-        base = input("Base: ")
-        height = input("Height: ")
-        triangle(base, height)
-    elif shape == "trapezoid":
-        top_base = input("Base One: ")
-        bottom_base = input("Base Two: ")
-        height = input("Height: ")
-        trapezoid(top_base, bottom_base, height)
-    elif shape == "circle":
-        radius = input("Radius: ")
-        circle(radius)
-    elif shape == "rectangular prism":
-        length = input("Length: ")
-        width = input("Width: ")
-        height = input("Height: ")
-        prism(length, width, height)
-    elif shape == "cone":
-        radius = input("Radius: ")
-        height = input("Height: ")
-        cone(radius, height)
-    elif shape == "cylinder":
-        radius = input("Radius: ")
-        height = input("Height: ")
-        cylinder(radius, height)
-    elif shape == "sphere":
-        radius = input("Radius: ")
-        sphere(radius)
+    if two_or_three == "2D":
+        two_id_calc(shape)
+    elif two_or_three == "3D":
+        three_id_calc(shape)
+    else:
+        print("\n Invalid Input \n")
+
+
+if __name__=="__main__": 
+    main()
+
+
+
+
 
     
