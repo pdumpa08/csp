235d8a2d48e4f795e999acba5556746435bb4884
jiayiqin625 <132865807+jiayiqin625@users.noreply.github.com>
2024-04-20 16:08:45 -0700

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -1,1 +1,15 @@
+import math
 
+def threeidcalc()
+
+def rectangle(length, width):
+    print("Area = Width * Height")
+    print(length * width)
+
+def trapezoid(topBase, bottomBase, height):
+    print("Area = [(topBase + bottomBase)/2] * Height")
+    print([(topBase + bottomBase)/2] * height)
+
+def circle(radius):
+    print("Area = (radius * radius) * π(3.14...)")
+    print(radius**2 * math.pi)
