a6b5c5ecc2e09edb34e4df1d9b395ddff59402dd
jiayiqin625 <132865807+jiayiqin625@users.noreply.github.com>
2024-04-20 16:15:56 -0700

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -1,7 +1,30 @@
 import math
 
-def threeidcalc()
+two_or_three = input("Would you like to analyze a 2D or 3D shape? ")
+shape = input("Which shape would you like to analyze? ")
+three_shapes = ["rectangular prism", "cone", "cylinder", "sphere"]
 
+def threeidcalc(shape):
+    for x in three_shapes:
+        if three_shapes[x] == shape:
+            if shape == "rectangular prism":
+                length = input("Length: ")
+                width = input("Width: ")
+                height = input("Height: ")
+                prism(length, width, height)
+            elif shape == "cone":
+                radius = input("Radius: ")
+                height = input("Height: ")
+                cone(radius, height)
+            elif shape == "cylinder":
+                radius = input("Radius: ")
+                height = input("Height: ")
+                cylinder(radius, height)
+            elif shape == "sphere":
+                radius = input("Radius: ")
+                sphere(radius)
+            
+                
 def rectangle(length, width):
     print("Area = Width * Height")
     print(length * width)
